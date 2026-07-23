9b11f3e2cd6bc27edc214472d9fce90e183e78fe
Russ Allbery <rra@lsst.org>
2026-07-23 11:01:52 -0700

    Move Butler creation into a dependency
    Create a new `butler_dependency` that creates a Butler for the
    collection and token of a request. Pass that into the request
    handler rather than the factory and strip the Butler creation out
    of the factory in preparation for repurposing the factory to create
    higher-level service objects with their per-request context.

--- src/sia/factory.py
+++ src/sia/factory.py
@@ -1,11 +1,9 @@
 """Component factory and process-wide status for sia."""
 
 import structlog
-from lsst.daf.butler import Butler, LabeledButlerFactory
 from structlog.stdlib import BoundLogger
 
 from .config import config
-from .models.data_collections import ButlerDataCollection
 
 __all__ = ["Factory"]
 
@@ -18,44 +16,12 @@ class Factory:
 
     Parameters
     ----------
-    labeled_butler_factory
-        The LabeledButlerFactory singleton
-    obscore_configs
-        The Obscore configurations
     logger
         The logger instance
     """
 
-    def __init__(
-        self,
-        labeled_butler_factory: LabeledButlerFactory,
-        logger: BoundLogger | None = None,
-    ) -> None:
-        self._labeled_butler_factory = labeled_butler_factory
+    def __init__(self, logger: BoundLogger | None = None) -> None:
         self._logger = logger or structlog.get_logger(config.name)
-
-    def create_butler(
-        self,
-        butler_collection: ButlerDataCollection,
-        token: str,
-    ) -> Butler:
-        """Create a Butler instance.
-
-        Parameters
-        ----------
-        butler_collection
-            The Butler data collection.
-        token
-            The token to use for the Butler instance.
-
-        Returns
-        -------
-        Butler
-            The Butler instance.
-        """
-        return self._labeled_butler_factory.create_butler(
-            label=butler_collection.name, access_token=token
-        )
 
     def set_logger(self, logger: BoundLogger) -> None:
         """Replace the internal logger.
